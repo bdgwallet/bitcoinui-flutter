34f936a9b401cccaa738410517bd1734f7312201
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2023-03-31 17:38:36 +0100

    Add UI Kit colors

--- lib/bitcoin_ui_kit.dart
+++ lib/bitcoin_ui_kit.dart
@@ -1,7 +1,42 @@
 library bitcoin_ui_kit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+extension BitcoinUIKitColors on Color {
+  // Primary and accent colors
+  Color get bitcoinOrange => const Color(0xFFF7931A);
+  Color get bitcoinRed => const Color(0xFFEB5757);
+  Color get bitcoinGreen => const Color(0xFF27AE60);
+  Color get bitcoinBlue => const Color(0xFF2D9CDB);
+  Color get bitcoinPurple => const Color(0xFFBB6BD9);
+
+  // Light theme neutrals
+  Color get bitcoinWhite => const Color(0xFFFFFFFF);
+  Color get bitcoinNeutral1 => const Color(0xFFF8F8F8);
+  Color get bitcoinNeutral2 => const Color(0xFFF4F4F4);
+  Color get bitcoinNeutral3 => const Color(0xFFEDEDED);
+  Color get bitcoinNeutral4 => const Color(0xFFDEDEDE);
+  Color get bitcoinNeutral5 => const Color(0xFFBBBBBB);
+  Color get bitcoinNeutral6 => const Color(0xFF999999);
+  Color get bitcoinNeutral7 => const Color(0xFF777777);
+  Color get bitcoinNeutral8 => const Color(0xFF404040);
+  Color get bitcoinBlack => const Color(0xFF000000);
+
+  // Dark primary and accent colors
+  Color get bitcoinOrangeDark => const Color(0xFFF89B2A);
+  Color get bitcoinRedDark => const Color(0xFFEC6363);
+  Color get bitcoinGreenDark => const Color(0xFF36B46B);
+  Color get bitcoinBlueDark => const Color(0xFF3CA3DE);
+  Color get bitcoinPurpleDark => const Color(0xFFC075DC);
+
+  // Dark theme neutrals
+  Color get bitcoinNeutral1Dark => const Color(0xFF1A1A1A);
+  Color get bitcoinNeutral2Dark => const Color(0xFF2D2D2D);
+  Color get bitcoinNeutral3Dark => const Color(0xFF444444);
+  Color get bitcoinNeutral4Dark => const Color(0xFF5C5C5C);
+  Color get bitcoinNeutral5Dark => const Color(0xFF787878);
+  Color get bitcoinNeutral6Dark => const Color(0xFF949494);
+  Color get bitcoinNeutral7Dark => const Color(0xFFB0B0B0);
+  Color get bitcoinNeutral8Dark => const Color(0xFFCCCCCC);
 }
