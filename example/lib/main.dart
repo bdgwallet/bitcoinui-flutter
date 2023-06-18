1b26db7c465feec44c0c3950c22f4dac126c2a8d
aniketambore <aniketambore0@gmail.com>
2023-06-18 14:50:57 +0530

    URL Strategy removing #

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:example/story_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   runApp(StoryApp());
 }
